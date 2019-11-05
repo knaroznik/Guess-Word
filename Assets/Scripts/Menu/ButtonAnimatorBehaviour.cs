52678cd5b39d3185f074a84c673f8ff913762bbd
knaroznik <Redivivatus@wp.pl>
2019-11-05 16:00:39 +0100

    [add] Menu animations.

---
+++ Assets/Scripts/Menu/ButtonAnimatorBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonAnimatorBehaviour : MonoBehaviour
+{
+    public float AnimationDelay;
+    public bool PlayAction = false;
+    public UnityEvent endAnimationAction;
+
+    Animator anim;
+
+    private IEnumerator Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetFloat("AnimationSpeed", 0f);
+        yield return new WaitForSeconds(AnimationDelay);
+        anim.SetFloat("AnimationSpeed", 1f);
+    }
+
+    
+
+    public void Play()
+    {
+        bool x = anim.GetBool("AnotherAnimation");
+        anim.SetBool("AnotherAnimation", !x);
+        PlayAction = true;
+    }
+
+    public void PlayAnimationOnly()
+    {
+        bool x = anim.GetBool("AnotherAnimation");
+        anim.SetBool("AnotherAnimation", !x);
+    }
+
+    public void PlayUnityAction()
+    {
+        if (PlayAction)
+        {
+            PlayAction = false;
+            endAnimationAction.Invoke();
+        }
+    }
+}
