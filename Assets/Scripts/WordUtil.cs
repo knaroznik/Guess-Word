4f589b0e7d8f79132f31e88ce2756327ccf01b76
knaroznik <Redivivatus@wp.pl>
2020-01-18 13:47:30 +0100

    [fix] Fixing dictionary input/output.

--- Assets/Scripts/WordUtil.cs
+++ Assets/Scripts/WordUtil.cs
@@ -6,6 +6,18 @@ using UnityEngine;
 
 public static class WordUtil
 {
+    private static List<string> definitionHolders = new List<string>() { "ds-list" , "ds-single"};
+    private static string notFoundKeyWord = "notFound";
+
+    private static string HolderDifinition(string rowRaw)
+    {
+        for(int i=0; i<definitionHolders.Count; i++)
+        {
+            if (rowRaw.Contains(definitionHolders[i])) return definitionHolders[i];
+        }
+        return notFoundKeyWord;
+    }
+
     public static string[] GetRandomWord()
     {
         string[] data = new string[2];
@@ -19,6 +31,10 @@ public static class WordUtil
             {
                 data[0] = GetStringRaw(rows[i]);
                 data[1] = GetWordDescription(data[0]);
+                if(data[1] == "")
+                {
+                    return GetRandomWord();
+                }
                 Debug.Log(data[0]);
                 break;
             }
@@ -31,6 +47,7 @@ public static class WordUtil
         string modifiedString = htmlRowResponse;
         modifiedString = modifiedString.Replace(" ", "");
         modifiedString = modifiedString.Replace("<p><ahref=\"/dictionary/", "");
+        modifiedString = modifiedString.Substring(0, modifiedString.Length - 1);
         return modifiedString;
     }
 
@@ -39,15 +56,21 @@ public static class WordUtil
         string output = "";
         WebClient client = new WebClient();
         string downloadedString = client.DownloadString("https://www.thefreedictionary.com/" + word);
-        string[] rows = downloadedString.Split(new string[] { "<div class" }, StringSplitOptions.None);
+        string[] rows = downloadedString.Split(new string[] { "<section data-src=" }, StringSplitOptions.None);
         for (int i = 0; i < rows.Length; i++)
         {
-            if (rows[i].Contains("ds-list"))
+            string holder = HolderDifinition(rows[i]);
+            if ( holder != notFoundKeyWord)
             {
-                rows[i] = rows[i].Replace("=\"ds-list\">", "");
-                rows[i] = rows[i].Replace("=\"syn ds-list\">", "");
-                rows[i] = rows[i].Split(new string[] { "</div>" }, StringSplitOptions.None)[0];
-                output += rows[i] + "\n";
+                string[] row = rows[i].Split(new string[] { holder }, StringSplitOptions.None);
+                for(int j=1; j<row.Length; j++)
+                {
+                    Debug.Log("OUTPUT Raw " + row[j]);
+                    int endPosition = row[j].IndexOf("</div>");
+                    row[j] = row[j].Substring(3, endPosition - 3);
+                    Debug.Log("OUTPUT " + row[j]);
+                    output += row[j] + "\n";
+                }
             }
         }
         return output;
